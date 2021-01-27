bb9db0c3f560ea7c2f6b64e91b3227bc11352a7c
ithinkso117 <ithinkso117@163.com>
2021-01-27 20:46:35 +0800

    Temp add tool

---
+++ jrl/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace jrl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********************************************************************");
+            Console.WriteLine("*                   Code generator for JsonRpcLite                  *");
+            Console.WriteLine("*********************************************************************");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: jrl -i [SMD-URL] -c [CODE] -o [OUTPUT]");
+                Console.WriteLine("       [SMD-URL] The url contains the service mapping description.");
+                Console.WriteLine("       [CODE] The code to generate, could be csharp, dart, javascript.");
+                Console.WriteLine("       [OUTPUT] The output folder to store the generated files.");
+            }
+            else
+            {
+                if (args.Contains("-i"))
+                {
+
+                }
+                if (args.Contains("-c"))
+                {
+
+                }
+                if (args.Contains("-o"))
+                {
+
+                }
+            }
+        }
+    }
+}
